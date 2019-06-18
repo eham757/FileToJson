58ebd26db806238921074acd5cf957ea2e9c8ea1
eham757 <eham757@gmail.com>
2019-06-18 15:58:52 +0200

    ExportModels

---
+++ File to json/Models/ExportModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace File_to_json.Models
+{
+    public class ExportModel
+    {
+        public int ID { get; set; }
+        public double y { get; set;  }
+        public double x { get; set; }
+    }
+}
